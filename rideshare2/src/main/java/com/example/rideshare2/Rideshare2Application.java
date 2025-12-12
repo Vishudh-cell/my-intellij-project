9b13569c37fb9fce88658b52960e5a5da2fef59a
Vishudh-cell <vishudh.24bcs10162@sst.scaler.com>
2025-12-12 23:58:35 +0530

    added rideshare2

---
+++ rideshare2/src/main/java/com/example/rideshare2/Rideshare2Application.java
@@ -0,0 +1,13 @@
+package com.example.rideshare2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rideshare2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rideshare2Application.class, args);
+	}
+
+}
